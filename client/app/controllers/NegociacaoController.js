d8257435023880c6e53971fda60f8b24bbac7e42
nathan.ramos <nathancostaand1@hotmail.com>
2019-06-10 19:57:24 -0300

    termino cap. 6

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -2,23 +2,32 @@ class NegociacaoController{
 
     constructor(){
         let $ = document.querySelector.bind(document);
-
+       
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._negociacoes = new Negociacoes();
     }
+
 
     adiciona(event){
         event.preventDefault();
+        this._negociacoes.adiciona(this._criarNegociacao());
+        this._limparFormulario();
+    }
 
-        let negociacao = new Negociacao(
+    _criarNegociacao(){
+        return new Negociacao = new Negociacao(
             DateConverter.paraData(this._inputData.value),
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
         );
+    }
 
-        console.log(DateConverter.paraTexto(negociacao.data));
-
-        console.log(negociacao);
+    _limparFormulario(){
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus()
     }
 }
--- client/app/ui/converters/DateConverter.js
+++ client/app/ui/converters/DateConverter.js
@@ -9,7 +9,7 @@ class DateConverter{
     }
     static paraData(texto){
 
-        if(!/^\d{4}-\d{2}=\d{2}$/.test(texot))
+        if(!/^\d{4}-\d{2}-\d{2}$/.test(texto))
             throw new Error('Deve estar no formato aaaa-mm-dd');
 
         return new Date(texto.split('-'));
