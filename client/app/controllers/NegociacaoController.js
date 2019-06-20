b4e999c5eb865f41ee4461a6a2bef34be8aa83fa
nathan.ramos <nathancostaand1@hotmail.com>
2019-06-19 23:14:06 -0300

    Termino cap. 16

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -10,19 +10,34 @@ class NegociacaoController {
         this._negociacoes = new Bind(new Negociacoes(),
             new NegociacoesView('#negociacoes')
             , 'adiciona', 'esvazia');
-
-
         this._mensagem = new Bind(new Mensagem()
             , new MensagemView('#mensagemView'), 'texto');
         this._service = new NegociacaoService();
+
+        this._init();
+    }
+
+    _init() {
+        getNegociacaoDao()
+            .then(dao => dao.listaTodos())
+            .then(negociacoes =>
+                negociacoes.forEach(negociacao =>
+                    this._negociacoes.adiciona(negociacao)))
+            .catch(err => this._mensagem.texto = err);
     }
 
     adiciona(event) {
         try {
             event.preventDefault();
-            this._negociacoes.adiciona(this._criarNegociacao());
-            this._mensagem.texto = 'Negociação adicionada com sucesso';
-            this._limparFormulario();
+
+            const negociacao = this._criarNegociacao();
+            getNegociacaoDao()
+                .then(dao => dao.adiciona(negociacao))
+                .then(() => {
+                    this._negociacoes.adiciona(negociacao);
+                    this._mensagem.texto = 'Negociacao adicionada com sucesso';
+                    this._limparFormulario();
+                }).catch(err => this.Mensagem.texto = err);
         } catch (err) {
             if (err instanceof DataInvalidaException) {
                 this._mensagem.texto = err.message;
@@ -36,17 +51,22 @@ class NegociacaoController {
         this._service.obtemNegociacoesDoPeriodo().then(
             negociacoes => {
                 negociacoes.filter(novaNegociacao => !this._negociacoes.paraArray()
-                    .some(negociacaoExistente => 
+                    .some(negociacaoExistente =>
                         novaNegociacao.equals(negociacaoExistente)))
-                        .forEach(negociacao =>
+                    .forEach(negociacao =>
                         this._negociacoes.adiciona(negociacao));
                 this._mensagem.texto = 'Negociações do período importadas com sucesso';
             }).catch(err => this._mensagem.texto = err);
     }
 
     apaga() {
-        this._negociacoes.esvazia();
-        this._mensagem.texto = 'Negociações apagadas com sucesso';
+        getNegociacaoDao()
+            .then(dao => dao.apagaTodos())
+            .then(() => {
+                this._negociacoes.esvazia();
+                this._mensagem.texto = 'Negociações apagadas com sucesso';
+
+            });
     }
 
     _criarNegociacao() {
